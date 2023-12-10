d9850cf089142c0f1462eaf69c771b37fb42c7c5
sramine03 <133198705+sramine03@users.noreply.github.com>
2023-12-09 21:52:06 -0500

    Create covid-19.py

---
+++ covid-19.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+df_covid=pd.read_csv("https://raw.githubusercontent.com/sramine03/Covid-19/main/covid_19_data.xlsx%20-%20covid_19_data.csv")
